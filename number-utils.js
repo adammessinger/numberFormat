901040d8411c88fc5d62b2867c4fc511a121447f
Adam Messinger <messinger06@gmail.com>
2015-03-30 15:55:11 -0500

    corrected spelling of "separator"

--- number-utils.js
+++ number-utils.js
@@ -7,22 +7,22 @@ var numberUtils = {
   // currency formatter taken from http://www.josscrowcroft.com/2011/code/format-unformat-money-currency-javascript/
   // default args: 0, 2, "$", ",", "."
   // TODO: permit reformatting currency strings by running thru unformatMoney 1st?
-  formatMoney: function(number, decimal_places, currency_symbol, thousands_seperator, decimal_seperator) {
+  formatMoney: function(number, decimal_places, currency_symbol, thousands_separator, decimal_separator) {
     number = number || 0;
     decimal_places = !isNaN(decimal_places = Math.abs(decimal_places)) ? decimal_places : 2;
     currency_symbol = currency_symbol !== undefined ? currency_symbol : '$';
-    thousands_seperator = thousands_seperator || ',';
-    decimal_seperator = decimal_seperator || '.';
+    thousands_separator = thousands_separator || ',';
+    decimal_separator = decimal_separator || '.';
 
     var negative = number < 0 ? '-' : '';
     var i = parseInt(number = Math.abs(+number || 0).toFixed(decimal_places), 10) + '';
     var j = i.length > 3 ? i.length % 3 : 0;
 
     return currency_symbol + negative
-      + (j ? i.substr(0, j) + thousands_seperator : '')
-      + i.substr(j).replace(/(\d{3})(?=\d)/g, '$1' + thousands_seperator)
+      + (j ? i.substr(0, j) + thousands_separator : '')
+      + i.substr(j).replace(/(\d{3})(?=\d)/g, '$1' + thousands_separator)
       + (decimal_places
-        ? decimal_seperator + Math.abs(number - i).toFixed(decimal_places).slice(2)
+        ? decimal_separator + Math.abs(number - i).toFixed(decimal_places).slice(2)
         : "");
   },
 
